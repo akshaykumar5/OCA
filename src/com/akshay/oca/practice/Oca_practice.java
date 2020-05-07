0c7e485669e2343b9e124d4331211aca78cf0c20
Akshay Kumar <akshay05.june@gmail.com>
2020-05-08 00:59:36 +0530

    Project initials

---
+++ src/com/akshay/oca/practice/Oca_practice.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akshay.oca.practice;
+
+/**
+ *
+ * @author akshay
+ */
+public class Oca_practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
